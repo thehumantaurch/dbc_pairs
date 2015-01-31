ac9a8c1f33c66f974e0dfde1123c362ec59bb2e4
Carl Schubert & Xiran Ou <pair+carlschubert.xiranou@devbootcamp.com>
2015-01-31 13:53:56 -0500

    remove validations from pair

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -2,23 +2,4 @@ class Pair < ActiveRecord::Base
   belongs_to :cohort
   belongs_to :first_student, class_name: "Student"
   belongs_to :second_student, class_name: "Student"
-
-  # validates :first_student, uniqueness: true
-
-  # validate :validate_different_students
-
-  # def validate_different_students
-  #   if first_student_id == second_student_id
-  #     errors.add(:first_student, "First and Second Student in pair cannot be the same")
-  #   end
-  # end
-
-
-# every student apears once in column 1 scope column 1
-# every student apears once in column 2
-# the leghth is == to the cohort
-# the count is as low as possible
-
-
-
 end
