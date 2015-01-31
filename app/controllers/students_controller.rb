b96d927ee7f808eeb178c0d5c6069227faf23715
Stephy B & Sarah <pair+barreyro.thehumantaurch@devbootcamp.com>
2015-01-31 16:47:19 -0500

    add student history view

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,9 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+    @pairs = Pair.where(first_student_id: @student.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
   get '/pairs' => 'pairs#index'
+  resources :students, only: :show
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
