ae9477c8cb834a04b85ba6b1d5bbf3921572dc8d
Carl Schubert & Xiran Ou <pair+carlschubert.xiranou@devbootcamp.com>
2015-01-31 13:54:52 -0500

    update initial pair generate

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -3,8 +3,9 @@ class Cohort < ActiveRecord::Base
   has_many :pairs
 
   def shuffle(number_of_people=2)
+    students = self.students
     [].tap do |pairs|
-      self.students.permutation(number_of_people) do |pair|
+      students.permutation(number_of_people) do |pair|
         pairs << pair.sort!
       end
     end.uniq!
@@ -12,25 +13,33 @@ class Cohort < ActiveRecord::Base
 
   def create_groups
     self.shuffle.each do |pair|
-      # student_one = Student.find_by(name: pair[0])
-      # student_two = Student.find_by(name: pair[1])
       pair = self.pairs.build(first_student_id: pair[0].id, second_student_id: pair[1].id)
       pair.save!
     end
   end
 
   def assign!
+    students = self.students
+    odd = students.last if students.size % 2 != 0
     pairs = self.pairs.to_a
     pairs.sort_by! do |pair|
       pair.counter
     end
+    picks_for_today = []
     until pairs.empty?
-      p pick = pairs.first
+      pick = pairs.first
+      picks_for_today << pick
       pairs.delete_if { |pair| pair.first_student_id==(pick.first_student_id) }
       pairs.delete_if { |pair| pair.first_student_id==(pick.second_student_id) }
       pairs.delete_if { |pair| pair.second_student_id==(pick.first_student_id) }
       pairs.delete_if { |pair| pair.second_student_id==(pick.second_student_id) }
+      pick.increment!(:counter)
     end
+    picks_for_today.collect! do |pick|
+      "#{Student.find(pick.first_student_id).name}, #{Student.find(pick.second_student_id).name}"
+    end
+    picks_for_today.sample << ", #{odd.name}"
+    p picks_for_today
   end
 
 end
