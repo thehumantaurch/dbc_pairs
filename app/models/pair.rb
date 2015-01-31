6d944c6f4932a14a275969cc60dabfe5af1e0f13
Stephy B & Sarah <pair+barreyro.thehumantaurch@devbootcamp.com>
2015-01-31 11:43:09 -0500

    add validations to pair model

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -2,4 +2,15 @@ class Pair < ActiveRecord::Base
   belongs_to :cohort
   belongs_to :first_student, class_name: "Student"
   belongs_to :second_student, class_name: "Student"
+
+  validates :first_student, uniqueness: true
+
+  validate :validate_different_students
+
+  def validate_different_students
+    if first_student_id == second_student_id
+      errors.add(:first_student, "First and Second Student in pair cannot be the same")
+    end
+  end
+
 end
