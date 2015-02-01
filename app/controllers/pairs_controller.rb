384485c638678c1f1192a26febbd70a6e0a1ea1d
Stephy B & Sarah <pair+barreyro.thehumantaurch@devbootcamp.com>
2015-01-31 19:00:36 -0500

    add link to pairs student page

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -3,12 +3,13 @@ class PairsController < ApplicationController
     cohort = Cohort.find(params[:cohort_id])
     cohort.create_groups
     assignment = cohort.assign!
-    @groups = formate_groups(assignment)
+    @pairs = formate_groups(assignment)
+    # @assignment = assignment
   end
 
   def formate_groups(assignment)
-    group_name_array = assignment[:groups].collect { |group| "#{Student.find(group.first_student_id).name}, #{Student.find(group.second_student_id).name}" }
-    group_name_array.sample << ", #{assignment[:odd].name}" if assignment[:odd]
-    group_name_array
+    pairs_array = assignment[:groups].collect { |group| [Student.find(group.first_student_id), Student.find(group.second_student_id)] }
+    pairs_array.sample << assignment[:odd] if assignment[:odd]
+    pairs_array
   end
 end
